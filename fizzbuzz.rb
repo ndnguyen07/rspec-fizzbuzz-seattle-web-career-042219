a109c00351c7d73110e6bc04cd9e6979de70dfef
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-12 02:52:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def Fizzbuzz(number)
-  if number / 3 && number / 5
+  if number % 3 && number % 5 == 0
     puts "Fizzbuzz"
-  elsif number / 3 
+  elsif number % 3 == 0
     puts "Fizz"
-  elsif number / 5
+  elsif number % 5 == 0
     puts "Buzz"
   else 
     puts "nil"
