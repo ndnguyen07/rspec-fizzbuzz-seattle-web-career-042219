b1d03dbc8e07183a33561394fabaf964e62de636
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-12 03:05:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def Fizzbuzz(number)
+def fizzbuzz(number)
   if number % 3 && number % 5 == 0
     puts "Fizzbuzz"
   elsif number % 3 == 0
