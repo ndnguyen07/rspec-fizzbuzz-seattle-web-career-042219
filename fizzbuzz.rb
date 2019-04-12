d554ca5a394aae01b5893d68b9a5fe64cae6fa4a
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-12 02:38:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
+def Fizzbuzz(number)
+  if number / 3 
+    puts "Fizz"
+  elsif number / 5
+    puts "Buzz"
+  elsif number / 3 && number / 5
+    puts "Fizzbuzz"
+  else 
+    puts "nil"
+end
